5c946b54618072b84c2de6cc4b02d9de229b67c3
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2024-08-22 07:22:19 +0600

    Added for front end

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Home</title>
+	<title>Property Rental as B&S | Home</title>
+	<!-- Tailwind Css CDN linking -->
+	<script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12">
-				<h1 style="text-align: center; padding: 100px 0;">Website is Under Constructing!!</h1>
+
+<body >
+
+  <header>
+    <div class="container px-10 py-5">
+		<div class="flex">
+			<div class="flex space-x-4">
+				<div><a href="tel:01731698756">+880 1731698756</a></div>
+				<div><a href="mailto:rentbuyhaven@gmail.com">rentbuyhaven@gmail.com</a></div>
 			</div>
+			<div>
+
+			</div>
+			
 		</div>
 	</div>
+  </header>
+
 </body>
+
 </html>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'media',
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
