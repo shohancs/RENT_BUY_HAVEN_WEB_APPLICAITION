e0915c1e4e4a4ab60b6d3f05114c1c62adffd84c
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2024-08-28 02:41:40 +0600

    Header part done

--- index.php
+++ index.php
@@ -26,5 +26,59 @@
                 </div>
             </div>
         </div>
+
+        <div class="main_navbar border">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg">
+                        <div class="container-fluid">
+                            <a class="navbar-brand" href="index.php"><img src="assets/images/logo2.png" alt=""></a>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
+                                <li class="nav-item mx-2">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                </li>                                
+                                <li class="nav-item dropdown mx-2">
+                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Services
+                                    </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><hr class="dropdown-divider"></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                                </li>
+                                <li class="nav-item mx-2">
+                                    <a class="nav-link" href="#">Blog</a>
+                                </li>
+                                <li class="nav-item mx-2">
+                                    <a class="nav-link" href="#">About Us</a>
+                                </li>
+                                <li class="nav-item mx-2">
+                                    <a class="nav-link" href="#">Sign In</a>
+                                </li>
+                                <li class="nav-item mx-3">
+                                    <a href="" class="btn btn-outline-warning btn-2 px-3">Join</a>
+                                </li>
+                                <li class="nav-item mx-3 d-flex align-items-center">
+                                    <div>
+                                    <a href=""><i class="fa-solid fa-cart-shopping cart"></i></a>
+                                    </div>
+                                    
+                                </li>
+                            </ul>
+                            </div>
+                        </div>
+                    </nav>
+                    </div>
+                </div>
+            </div>
+        </div> 
+        
     </header>
 <?php include"inc/footer.php"; ?>
