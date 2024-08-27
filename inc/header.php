6176616b31f3b6f1e7f366dd0de72281ac2a8ad5
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2024-08-27 09:53:13 +0600

    Front end Top header part done

--- inc/header.php
+++ inc/header.php
@@ -1,47 +1,26 @@
 <?php
     ob_start();
-    include("admin/inc/db.php");
+    include"admin/inc/db.php"
 ?>
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Rent Buy Haven | Home</title>
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta name="description" content="multikart">
-    <meta name="keywords" content="multikart">
-    <meta name="author" content="multikart">
-    <link rel="shortcut icon" href="assets/images/favIcon.png" type="image/x-icon">
-    <title>Multikart - Multi-purpose E-commerce Html Template</title>
-
-    <!--Google font-->
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Yellowtail&display=swap" rel="stylesheet">
-
-    <!-- Icons -->
-    <link rel="stylesheet" type="text/css" href="assets/css/vendors/font-awesome.css">
-
-    <!--Slick slider css-->
-    <link rel="stylesheet" type="text/css" href="assets/css/vendors/slick.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/vendors/slick-theme.css">
-
-    <!-- Animate icon -->
-    <link rel="stylesheet" type="text/css" href="assets/css/vendors/animate.css">
-
-    <!-- Themify icon -->
-    <link rel="stylesheet" type="text/css" href="assets/css/vendors/themify-icons.css">
-
-    <!-- Bootstrap css -->
-    <link rel="stylesheet" type="text/css" href="assets/css/vendors/bootstrap.css">
-
-    <!-- Font Awesome cdn link -->
+    <!-- Font Awesome CDN link Here -->
     <script src="https://kit.fontawesome.com/0c66e46c25.js" crossorigin="anonymous"></script>
 
-    <!-- Theme css -->
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-</head>
+    <!-- Google Font Link Here -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
-<body class="theme-color-1"></body>
+    <!-- Custom Css Link Here -->
+     <link rel="stylesheet" href="inc/custom.css">
+  </head>
+
+  <body></body>
