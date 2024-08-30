59b9ac85fa514ca92be5ffb1e6c2935ef5bbc417
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2024-08-30 18:45:34 +0600

    Our services part done

--- index.php
+++ index.php
@@ -35,6 +35,49 @@
      </section>
     <!-- End: Bannner Carousel Slider -->
 
+    <!-- START: Our Services -->
+     <section class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="text-center fs-1 fw-semibold pt-3 " style="color:#023021; letter-spacing: 1px; ">Our Services</h1>
+                    <div class="mb-3" style="border-bottom: 3px solid #ffc107;width: 5%;margin: 0px auto;"></div>
+
+                    <div class="row py-5">
+                        <div class="col-lg-12">
+                            <div class="d-flex justify-content-center">
+                                <a href="">
+                                    <div class="card me-5 py-3 service-card" style="width: 18rem;">
+                                    <div class="background-image"></div>
+                                        <div class="card-body">                                        
+                                            <img src="assets/images/rent.svg" alt="" class="w-50" style="margin:0 23%;">
+                                            <h1 class="text-center pt-3 card-head" style="color:#023021;">Rent</h1>
+                                            <p class="card-text text-center">Affordable housing options<br>Find your ideal place today!<br>Explore our listings. <br><span class="text-primary">Click Here..</span></p>
+                                        </div>
+                                    </div>
+                                </a>
+                                
+                                <a href="">
+                                    <div class="card me-5 py-2 service-card" style="width: 18rem;">
+                                        <div class="card-body">
+                                            <img src="assets/images/buy.svg" alt="" class="w-50" style="margin:0 23%;">
+                                            <h1 class="text-center pt-3" style="color:#023021;">Buy</h1>
+                                            <p class="card-text text-center">Affordable housing options<br>Find your ideal place today!<br>Explore our listings. <br><span class="text-primary">Click Here..</span></p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </section>
+    <!-- END: Our Services -->
+
+
+    
+
     
 </main>
     
