9d3d0747de64cbab9542d7b9e57c5f4130d82fb7
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2024-10-29 19:45:57 +0600

    Banner Part Done

--- inc/topheader.php
+++ inc/topheader.php
@@ -6,7 +6,7 @@
                     <nav class="navbar navbar-expand-lg">
                         <div class="container-fluid">
 
-                            <a class="navbar-brand" href="index.php"><img src="assets/images/logo2.png" alt="" style="width: 68%;"></a>
+                            <a class="navbar-brand" href="index.php"><img src="assets/images/logo2.png" alt="" style="width: 70%;"></a>
                             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                             </button>
@@ -43,13 +43,13 @@
                                 <li class="nav-item">
                                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                                 </li>
-                                <li class="nav-item">
+                                <li class="nav-item mx-4">
                                     <a class="nav-link" href="#">Rent</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="#">Buy</a>
                                 </li>
-                                <li class="nav-item">
+                                <li class="nav-item mx-4">
                                     <a class="nav-link" href="#">Blog</a>
                                 </li>
                                 <li class="nav-item">
